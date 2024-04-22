32d4627afd35f71fb5edc2232f2a8ce9edbbbdea
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2024-04-22 10:15:21 +0200

    NoteBuilder corrected

--- components/notes/noteBuilder.js
+++ components/notes/noteBuilder.js
@@ -1,24 +1,35 @@
 
 export function textBuilder(NotesString, Timestamp, parentDOM) {
 
-    let noteContainer = document.createElement("div")
+    console.log(NotesString);
+
+
+    let note_Time_Container = document.createElement("div")
     let TimestampDiv = document.createElement("div")
-    let notesDiv = document.createElement("div");
-    parentDOM.append(noteContainer)
-    noteContainer.append(TimestampDiv)
-    noteContainer.append(notesDiv)
+    let noteContainer = document.createElement("div")
+
+    parentDOM.append(note_Time_Container)
+    note_Time_Container.append(TimestampDiv)
+    note_Time_Container.append(noteContainer)
+
+    NotesString.forEach(note => {
+        let notesDiv = document.createElement("div");
+        noteContainer.append(notesDiv)
+        notesDiv.innerHTML = `<p>${note}</p>`;
+        notesDiv.classList.add("notes")
+    });
+
 
     const date = new Date(Timestamp)
     const hours = date.getHours().toString().padEnd(0, '0')
     const minutes = date.getMinutes().toString().padStart(2, '0')
     const Time = (`${hours}:${minutes}`);
-    console.log(Time);
 
-    notesDiv.innerHTML = `<p>${NotesString}</p>`;
+
     TimestampDiv.innerHTML = `<p>${Time}</p>`
+    note_Time_Container.classList.add("note_Time_Container")
     noteContainer.classList.add("noteContainer")
     TimestampDiv.classList.add("time")
-    notesDiv.classList.add("notes")
 
 }
 
