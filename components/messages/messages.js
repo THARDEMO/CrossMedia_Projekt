635007925b1137eac5c3fc0a1c8d994fea3bdd02
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2024-04-22 11:43:38 +0200

    Message Page

--- components/messages/messages.js
+++ components/messages/messages.js
@@ -1,5 +1,6 @@
 import * as cManager from '../cManager.js'
 import * as STATE from '../../Logic/state.js';
+import { textBuilder } from "./messageBuilder.js";
 
 export const component = {
     domID: 'Messages',
@@ -14,6 +15,26 @@ async function render(DOM) {
     DOM.innerHTML = component.domID;
 
     const messages = await STATE.Get({ entity: 'messages', id: STATE.currentUserID() });
+
+    let conversations = [{
+        ToUser: ["hello where are u?",
+            "how are u?",
+            "Awnser!"],
+        FromUser: ["im at home was just gonna take a nap!", "what do you want?"],
+        timestamp: 1713769273,
+    },
+    {
+        ToUser: ["hello where are u?"],
+        FromUser: ["im at home was just gonna take a nap!"],
+        timestamp: 1713769273,
+    },
+    {
+        ToUser: ["hello where are u?"],
+        FromUser: ["im at home was just gonna take a nap!"],
+        timestamp: 1713769273,
+    }]
+
+    textBuilder(conversations, DOM)
 
     console.log(messages);
 }
