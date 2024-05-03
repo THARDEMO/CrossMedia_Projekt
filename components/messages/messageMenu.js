941817eff2afce2870965ebe45c236c81fc636f8
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2024-05-03 13:47:12 +0200

    Minor css fixes

--- components/messages/messageMenu.js
+++ components/messages/messageMenu.js
@@ -7,7 +7,7 @@ export function displayMenu(Messages, parentDOM) {
         let displayedMessage = document.createElement("div")
         displayedMessage.classList.add("displayedMessages")
         let messagePreview = message.conversation[0].ToUser[0]
-        displayedMessage.innerHTML = `<p>${message.name}</p> <p>${messagePreview.substring(0, 30)}...</p>`
+        displayedMessage.innerHTML = `<div id="messenger"><p>${message.name}</p></div> <p>${messagePreview.substring(0, 30)}...</p>`
         parentDOM.append(displayedMessage)
 
         router('messageDisplay', displayedMessage, `message_name=${message.name}`);
