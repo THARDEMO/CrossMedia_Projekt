06680e909c0fbc5c1ac6f2ef11ab8e995b7ac9fe
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2024-05-16 12:03:48 +0200

    .

--- components/messageDisplay/messagesDisplay.js
+++ components/messageDisplay/messagesDisplay.js
@@ -20,7 +20,6 @@ async function render(DOM) {
     const urlParams = new URLSearchParams(window.location.search);
     const name = urlParams.get('name');
     const messenger_id = urlParams.get(`message_id`)
-    console.log(messenger_id);
 
     const messages = await STATE.Get(
         {
@@ -35,7 +34,29 @@ async function render(DOM) {
     DOM.append(nameDiv)
 
 
-    if (messages.length > 0) {
+    if (messenger_id === "1") {
+        console.log("hej");
+        let message_time_container = document.createElement("div")
+        let TimestampDiv = document.createElement("div")
+        let messageContainer = document.createElement("div")
+
+        DOM.append(message_time_container)
+        message_time_container.append(TimestampDiv)
+        message_time_container.append(messageContainer)
+
+        TimestampDiv.innerHTML = `<p>igår 22:05</p>`
+        message_time_container.classList.add("message_time_Container")
+        messageContainer.classList.add("messageContainer")
+        TimestampDiv.classList.add("messagesTime")
+
+        let messageToUserDiv = document.createElement("div");
+        messageContainer.append(messageToUserDiv)
+        messageToUserDiv.innerHTML = `<p>När är du hemma?</p>`;
+        messageToUserDiv.classList.add("messageToUser")
+    }
+
+
+    if (messages.length > 0 || messenger_id === "1") {
         messages.forEach(message => {
             console.log("message", messages);
             message.conversation.sort((a, b) => a.timestamp - b.timestamp);
@@ -45,6 +66,13 @@ async function render(DOM) {
     } else {
         DOM.innerHTML = `<p id="NoMessages">You have no messages.</p>`
     }
+
+
+    let parentNode = document.querySelector(`#messageDisplay`)
+    let specificChildNode = parentNode.childNodes[1];
+    parentNode.insertBefore(specificChildNode, null)
+
+
 
 
 }
